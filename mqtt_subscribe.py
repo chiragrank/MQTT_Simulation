111ae128d4425d65b069996da91b63a88344ec79
Chirag Rank <chiiragrank@gmail.com>
2022-03-09 12:32:44 -0600

    Update publish and subscribe scripts to publish and listen on multiple ports on the local host

--- mqtt_subscribe.py
+++ mqtt_subscribe.py
@@ -4,16 +4,8 @@ import random
 
 from paho.mqtt import client as mqtt_client
 
-broker = "broker.emqx.io"
-port = 1883
-topic = "python/mqtt"
-# generate client ID with pub prefix randomly
-client_id = f"python-mqtt-{random.randint(0, 100)}"
-# username = 'emqx'
-# password = 'public'
 
-
-def connect_mqtt() -> mqtt_client:
+def connect_mqtt(client_id, broker, port) -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
@@ -21,23 +13,28 @@ def connect_mqtt() -> mqtt_client:
             print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
-    client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
 
 
-def subscribe(client: mqtt_client):
+def subscribe(client: mqtt_client, topic_list):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
 
-    client.subscribe(topic)
+    for topic in topic_list:
+        client.subscribe(topic)
     client.on_message = on_message
 
 
 def run():
-    client = connect_mqtt()
-    subscribe(client)
+    broker = "127.0.0.1"
+    port = 1883
+    topic_list = ["python/mqtt", "java/mqtt"]
+    client_id = f"python-mqtt-{random.randint(0, 100)}"
+
+    client = connect_mqtt(client_id, broker, port)
+    subscribe(client, topic_list)
     client.loop_forever()
 
 
